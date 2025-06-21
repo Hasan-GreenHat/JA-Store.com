f134ebc2f0081d5a5b454112d854db7b7c28de95
Hasan-GreenHat <ahmedjunaid786114@gmail.com>
2025-06-21 22:41:45 +0530

    Add files via upload

---
+++ PHONE.js
@@ -0,0 +1,10 @@
+var a;
+function search() 
+{
+    if(a==1)
+    {
+      document.getElementById("searchbar").type="search";
+      document.getElementById("searchicon").src=<i class="fa-solid fa-magnifying-glass" id="searchicon"></i>;
+      a==0;
+    }
+}
